ae56a65e1d21004e5076352d0fc57b20b36cca20
Jakob Prossinger <jakob.prossinger@gmail.com>
2021-11-14 15:53:02 +0100

    added .gitignore

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from INA260_MINIMAL import INA260
 DEVICE_BUS = 1
 DEVICE_ADDR = 0x40
 
+
 def main():
-	ina260 = INA260(DEVICE_ADDR)
-	ina260.reset_chip()
-
-if __name__ == '__main__':  
-   main()
+    ina260 = INA260(DEVICE_ADDR)
+    ina260.reset_chip()
+    print(ina260.read_configuration_register())
 
 
+if __name__ == '__main__':
+    main()
